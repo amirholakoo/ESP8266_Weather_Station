a30dee749c4bbc1b97254277ee8b0412a529a146
amirholakoo <103663692+amirholakoo@users.noreply.github.com>
2023-04-14 07:21:19 -0700

    Create post-data.php

---
+++ post-data.php
@@ -0,0 +1,52 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "XXXXXXXX";
+$dbname = "Weather";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the table exists, and if not, create it
+$table_check = "SHOW TABLES LIKE 'weather_data'";
+$result = $conn->query($table_check);
+
+if ($result->num_rows == 0) {
+  $create_table = "CREATE TABLE weather_data (
+  id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  temperature FLOAT NOT NULL,
+  humidity FLOAT NOT NULL,
+  pressure FLOAT NOT NULL,
+  lux INT(11) NOT NULL,
+  mq135 INT(11) NOT NULL,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+
+
+  if ($conn->query($create_table) === TRUE) {
+    echo "Table 'weather_data' created successfully";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
+}
+
+$temperature = $_POST['temperature'];
+$humidity = $_POST['humidity'];
+$pressure = $_POST['pressure'];
+$lux = $_POST['lux'];
+$mq135 = $_POST['mq135'];
+
+$sql = "INSERT INTO weather_data (temperature, humidity, pressure, lux, mq135) VALUES ('$temperature>
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
